a819097e5e13cff1ab5751ff0259d91f56987341
AndreAMolkov <molkov_aa@mail.ru>
2017-10-31 17:28:51 +0300

    task2 begin

--- task2/task2.c
+++ task2/task2.c
@@ -1,5 +1,18 @@
 #include "task2.h"
+#include<stdio.h>
 char* greet(int hour, int min)
 {
-	// здесь реализация
+	char* greeting;
+	if (hour > 24 )
+		greeting = "Uncorrect time!";
+	if (hour >= 00 && hour < 6)
+		greeting = "Good night!";
+	if (hour >= 6 && hour < 11)
+		greeting = "Good morning!";
+	if (hour >= 11 && hour < 18)
+		greeting = "Good day!";
+	if (hour >= 18 && hour < 00)
+		greeting = "Good evening!";
+	
+	return greeting;
 }
